63c7af3a0464cf14ddc59945823afe2445a36a2c
Your Name <you@example.com>
2023-04-22 18:44:32 +0600

    feat/fix/addreactslick

--- src/components/main/placeOfChoice/PlaceOfChoice.jsx
+++ src/components/main/placeOfChoice/PlaceOfChoice.jsx
@@ -1,21 +1,69 @@
-import React from "react";
+import React, { Component, useRef } from "react";
+import Slider from "react-slick";
 import css from "./PlaceOfChoice.module.scss";
-import mainImg1 from "../../../assets/mainImg1.avif";
-import mainImg3 from "../../../assets/mainImg3.jpg";
+import mainImg from "../../../assets/mainImg3.jpg";
+import click from "../../../assets/click.png";
 
-function PlaceOfChoice() {
+const PlaceOfChoice = () => {
+  const arrowRef = useRef(null);
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    arrows: false,
+  };
   return (
-    <div className={css.choiceContent}>
-      <div className={css.firstContent}>
-        <h1>Строительные материалы</h1>
-        <img src={mainImg1} alt="" />
-      </div>
-      <div className={css.secondContent}>
-        <h1>Cантехнические материалы</h1>
-        <img src={mainImg3} alt="" />
-      </div>
+    <div className={css.main}>
+      <button
+        className={css.prevClick}
+        onClick={() => arrowRef.current.slickNext()}
+      >
+        <img src={click} alt="" />
+      </button>
+      <Slider ref={arrowRef} {...settings}>
+        <div className={css.mainImg}>
+          <h3 className={css.mainImgContents}>
+            <img src={mainImg} alt="" />
+          </h3>
+        </div>
+        <div className={css.mainImg}>
+          <h3 className={css.mainImgContents}>
+            <img src={mainImg} alt="" />
+          </h3>
+        </div>
+        <div className={css.mainImg}>
+          <h3 className={css.mainImgContents}>
+            <img src={mainImg} alt="" />
+          </h3>
+        </div>
+        <div className={css.mainImg}>
+          <h3 className={css.mainImgContents}>
+            <img src={mainImg} alt="" />
+          </h3>
+        </div>
+        <div className={css.mainImg}>
+          <h3 className={css.mainImgContents}>
+            <img src={mainImg} alt="" />
+          </h3>
+        </div>
+        <div className={css.mainImg}>
+          <h3 className={css.mainImgContents}>
+            <img src={mainImg} alt="" />
+          </h3>
+        </div>
+      </Slider>
+      <button
+        className={css.nextClick}
+        onClick={() => arrowRef.current.slickPrev()}
+      >
+        <img src={click} alt="" />
+      </button>
     </div>
   );
-}
+};
 
 export default PlaceOfChoice;
