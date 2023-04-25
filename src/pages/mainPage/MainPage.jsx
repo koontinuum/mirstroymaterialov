eee4048e5234a41b3329416c87ab281fe8c4bf9e
Your Name <you@example.com>
2023-04-25 18:00:22 +0600

    fix/mainPage

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -4,9 +4,6 @@ import Header from "../../components/header/Header";
 import CardsInfo from "../../components/CardsInfo/CardsInfo";
 import Category from "../../components/Category/Category";
 import Card from "../../components/card/Card";
-import Footer from "../../components/footer/Footer";
-=======
-import Header from "../../components/header/Header";
 import GifInfo from "../../components/gifInfo/GifInfo";
 import PlaceOfChoice from "../../components/placeOfChoice/PlaceOfChoice";
 
@@ -14,15 +11,10 @@ function MainPage() {
   return (
     <div>
       <Header />
-      <Category/>
-      <Main />
-      <Card/>
-      <GifInfo/>
-      <Footer/>
-      <PlaceOfChoice />
       <GifInfo />
       <PlaceOfChoice />
-      {/* <Card /> */}
+      <Category />
+      <Card />
     </div>
   );
 }
