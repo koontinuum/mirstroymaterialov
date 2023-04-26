31fb55d598f928b729573f28e15e2e0901a3c800
arsczx <arsendjangrachev@gmail.com>
2023-04-26 17:33:10 +0600

    feat/addicons

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,11 +1,7 @@
 import React from "react";
-import css from "./MainPage.module.scss";
-import Header from "../../components/header/Header";
 import CardsInfo from "../../components/CardsInfo/CardsInfo";
 import Category from "../../components/Category/Category";
 import Card from "../../components/card/Card";
-import Footer from "../../components/footer/Footer";
-=======
 import Header from "../../components/header/Header";
 import GifInfo from "../../components/gifInfo/GifInfo";
 import PlaceOfChoice from "../../components/placeOfChoice/PlaceOfChoice";
@@ -15,13 +11,9 @@ function MainPage() {
     <div>
       <Header />
       <Category/>
-      <Main />
       <Card/>
-      <GifInfo/>
-      <Footer/>
       <PlaceOfChoice />
       <GifInfo />
-      <PlaceOfChoice />
       {/* <Card /> */}
     </div>
   );
