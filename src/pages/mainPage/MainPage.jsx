c9a36304c84fbdb0af28d127d7d9b8299afa4e01
daniihehyhe <115088264+daniihehyhe@users.noreply.github.com>
2023-04-26 18:29:21 +0600

    get change

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import Header from "../../components/header/Header";
 import Category from "../../components/Category/Category";
 import Card from "../../components/card/Card";
+import CardsInfo from "../../components/CardsInfo/CardsInfo";
 import Footer from "../../components/footer/Footer";
 import GifInfo from "../../components/gifInfo/GifInfo";
 import PlaceOfChoice from "../../components/placeOfChoice/PlaceOfChoice";
 import useSearch from "../../hooks/UseSearch";
 
 function MainPage() {
-   const { openCard } = useSearch();
-   console.log(openCard)
+  const { openCard } = useSearch();
+  console.log(openCard);
   return (
     <div>
       <Header />
@@ -18,9 +19,9 @@ function MainPage() {
           <Card />
         </div>
       )}
-      <Category />
-
       <PlaceOfChoice />
+      <Category />
+      <CardsInfo />
       <GifInfo />
       <Footer />
     </div>
